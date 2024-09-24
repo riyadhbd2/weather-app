bcb7085bff9747acf5afecb88060e3a0b613d9df
riyadhbd2 <riyadhbd2@gmail.com>
2024-09-24 10:31:03 +0200

    weather api connection done

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,10 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import clear_icon from "../assets/clear.png";
 import humidity_icon from "../assets/humidity.png";
 import search_icon from "../assets/search.png";
 import wind_icon from "../assets/wind.png";
+import cloud_icon from "../assets/cloud.png";
+import rain_icon from "../assets/rain.png";
+import snow_icon from "../assets/snow.png";
+import drizzle_icon from "../assets/drizzle.png";
 
 const Weather = () => {
+
+    const [weatherData, setWeatherData] = useState(false);
+
+    const allIcons ={
+        "01d": clear_icon,
+        "01n" : clear_icon,
+        "02d" : cloud_icon,
+        "02n" : cloud_icon,
+        "03d" : cloud_icon,
+        "03n" : cloud_icon,
+        "04d" : drizzle_icon,
+        "04n" : drizzle_icon,
+        "09d" : rain_icon,
+        "09n" : rain_icon,
+        "10d" : rain_icon,
+        "10n" : rain_icon,
+        "13d" : snow_icon,
+        "13n" : snow_icon
+
+    }
+
+    const search = async (city) =>{
+        try {
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`;
+
+            const response = await fetch(url);
+            const data = await response.json();
+            console.log(data);
+            const icon = allIcons[data.weather[0].icon] || clear_icon;
+            setWeatherData({
+                humidity: data.main.humidity,
+                windSpeed: data.wind.speed,
+                temperature: Math.floor(data.main.temp),
+                location: data.name,
+                icon: icon
+            });
+        } catch (error) {
+            
+        }
+    }
+
+    useEffect(()=>{
+        search("London")
+    },[])
   return (
     <div className="place-self-center p-10 rounded-lg bg-gradient-to-r from-violet-500 to-fuchsia-500 items-center">
       <div className="flex items-center gap-3">
@@ -20,24 +68,24 @@ const Weather = () => {
         />
       </div>
       <div className="grid items-center justify-center mt-5">
-        <img className="w-28" src={clear_icon} alt="" />
+        <img className="w-28" src={weatherData.icon} alt="" />
         <div className="flex flex-col items-center justify-center mt-5">
-          <p className="text-white text-3xl font-sm leading-7">16°C</p>
-          <p className="text-white text-xs ">London</p>
+          <p className="text-white text-3xl font-sm leading-7">{weatherData.temperature}<span className="text-3xl">°C</span></p>
+          <p className="text-white text-sm ">{weatherData.location}</p>
         </div>
       </div>
       <div className="w-full mt-10 text-white flex justify-between items-center">
         <div className="flex gap-3 text-sm">
           <img className="w-6 mt-1" src={humidity_icon} alt="" />
           <div>
-            <p>91 %</p>
+            <p>{weatherData.humidity} %</p>
             <span>Humidity</span>
           </div>
         </div>
         <div className="flex gap-3 text-sm">
           <img src={wind_icon} alt="" />
           <div>
-            <p>3.6 km/h</p>
+            <p>{weatherData.windSpeed} km/h</p>
             <span>Wind Speed</span>
           </div>
         </div>
